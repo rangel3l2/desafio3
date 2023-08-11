b9ca144329e4d1a29eddf83cbec120b11a06bfc2
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2023-08-11 12:31:03 -0400

    upd vinicius

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import numpy as np
-import csv
 from utils_functions.write_csv import write_csv
-from utils_functions.read_csv import read_csv
+from utils_functions.read_csv import *
 from utils_functions.calculate_angle import calculate_angle
 
 def is_rectangle(dots):
    
     list_of_rectangles = []
-    margin_percent = 3
+    margin_percent = 30
     for dot_set in dots:
         pA =[dot_set['x_A'], dot_set['y_A']]
         pB =[dot_set['x_B'], dot_set['y_B']]
@@ -19,44 +17,55 @@ def is_rectangle(dots):
         D = calculate_angle(pC, pD, pA)
         
         if((A + B + C + D) == 360):      
-                  
             if(90 - margin_percent <= A <= 90 + margin_percent and 90 - margin_percent <= B <= 90 + margin_percent and 90 - margin_percent <= C <= 90 + margin_percent and 90 - margin_percent <= D <= 90 + margin_percent):
                 dot_set['is_rectangle'] = True
-                
                 list_of_rectangles.append(dot_set)
-                
-                
             else:
-            
                 dot_set['is_rectangle'] = False
                 list_of_rectangles.append(dot_set)
         else:
             dot_set['is_rectangle'] = False
             list_of_rectangles.append(dot_set)
+
     for index, data in enumerate(list_of_rectangles):
         data['number_rectangle'] = f'Retângulo {index}'
             
+    
     return list_of_rectangles
+
+
 def resulteds(data):
     is_rect = 0
     non_react = 0
+    
     for item in data:
         if item['is_rectangle'] == True:
-            print(f"{item['number_rectangle']} is rectangle")
+            # print(f"{item['number_rectangle']} is rectangle")
             is_rect += 1 
         else: 
-            print(f"{item['number_rectangle']} is not rectangle")            
+            # print(f"{item['number_rectangle']} is not rectangle")            
             non_react += 1
-    print(f'Quantidade de retangulos verdadeiros: {is_rect}')
-    print(f'Quantidade de retangulos falsos: {non_react}')
-    
+
+    print(f'Quantidade de retangulos verdadeiros com margem: {is_rect}')
+    # print(f'Quantidade de retangulos falsos: {non_react}')
+
+    acertos = (is_rect/len(data)) * 100    
+    return acertos
         
 
 def main():
-        
     dots = read_csv('casos.csv')
+    
+    linhas = count_csv_lines('casos.csv')
+    acertos_banco = cont_csv_condition('casos.csv')
+    print(f'Quantidade de retangulos do arquivo de base: {acertos_banco}')
+    percent = (acertos_banco/linhas) * 100
+
     data_with_margin = is_rectangle(dots)
     write_csv(data_with_margin)
-    resulteds(data_with_margin)
+    acertos = resulteds(data_with_margin)
+
+    print(f'banco = {percent:.2f}% com margem = {acertos:.2f}% diferença de {abs(acertos - percent):.2f}%')
+
 
 main()
--- utils_functions/read_csv.py
+++ utils_functions/read_csv.py
@@ -1,4 +1,5 @@
 import csv
+
 def read_csv(csv_file):
     dots = []
     with open(csv_file, 'r') as csv_file:
@@ -7,9 +8,25 @@ def read_csv(csv_file):
         
         for line in csv_reader:
             dot_dict = {}
-            for i, header in enumerate(headers):                  
+            for i, header in enumerate(headers):           
                 if header != 'rectangle' and header != 'is_rectangle':
                     dot_dict[header] = float(line[i])
             dots.append(dot_dict)
             
     return dots
+
+def count_csv_lines(csv_file):
+    with open(csv_file, mode="r") as file:
+        reader = csv.reader(file)
+        num_lines = sum(1 for _ in reader)
+    return num_lines - 1
+
+def cont_csv_condition(csv_file):
+    cont = 0
+    with open(csv_file, mode="r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == 'True':
+                cont += 1
+    
+    return cont
